7d285a77f51255f117de94c665ffd646ae7ee778
Danny Lee <danny@dongwei.li>
2020-08-08 17:12:30 -0700

    Setup ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended", // This must be the last plugin
+  ],
+  rules: {},
+}
